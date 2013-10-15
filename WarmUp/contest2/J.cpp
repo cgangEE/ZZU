d01b73c5853939d5cdbef2039b7e11524d2ebe00
cgangEE <cgangee@gmail.com>
2013-10-15 18:06:27 +0800

    .

--- WarmUp/contest2/J.cpp
+++ WarmUp/contest2/J.cpp
@@ -1,106 +1,58 @@
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <climits>
-#include <cctype>
-#include <ctime>
-
 #include <algorithm>
 #include <iostream>
-#include <queue>
-#include <deque>
-#include <vector>
-#include <set>
-#include <map>
-#include <stack>
 using namespace std;
-typedef vector<int> vi;
-typedef map<int,int> mii;
-typedef long long ll;
-template <class T> void checkmax(T &t, T x){if (x > t) t = x;}
-template <class T> void checkmin(T &t, T x){if (x < t) t = x;}
-template <class T> void _checkmax(T &t, T x){if (t == -1 || x > t) t = x;}
-template <class T> void _checkmin(T &t, T x){if (t == -1 || x < t) t = x;}
-#define INF (INT_MAX/10)
-#define SQR(x) ((x)*(x))
-#define rep(i, n) for (int i=0; i<(n); ++i)
-#define repf(i, a, b) for (int i=(a); i<=(b); ++i)
-#define repd(i, a, b) for (int i=(a); i>=(b); --i)
-#define iter(v) __typeof((v).begin())
-#define foreach(it, v) for (iter(v) it = (v).begin(); it != (v).end(); it++)
-#define clr(ar,val) memset(ar, val, sizeof(ar))
-#define pb push_back
-#define sz(a) ((int)(a).size())
-#define all(a) a.begin(), a.end()
-#define mid(x, y) ((x+y)/2)
-#define vp vector<P>
-#define itr iterator
-#define N 50000
-#define M 100
+const int fin=0, maxn=50010;
+struct rec{int w,h;} a[maxn];
+int n,tn,limit;
+long long w[maxn], h[maxn], f0[maxn], f1[maxn];
+int q[maxn];
 
-struct P{
-	int x, y;
-	void in(){ scanf("%d%d", &x, &y); }
-	bool operator <(const P&p)const{
-		if (x!=p.x) return x<p.x;
-		return y<p.y;
+inline bool cmp(rec a, rec b){
+	if (a.w==b.w) return a.h<b.h; return a.w>b.w;
+}
+
+void init(){
+	int i, maxh;
+	for (i=1; i<=tn; ++i) scanf("%d%d", &a[i].w, &a[i].h);
+	std::sort(a+1, a+tn+1, cmp);
+	maxh=-1; n=0; a[tn+1].w=-1;
+	for (i=1; i<=tn; ++i)
+		if (a[i].w!=a[i+1].w)
+			if (a[i].h>maxh){
+				maxh=a[i].h; n+=1;
+				h[n]=a[i].h; w[n]=a[i].w;
+			}
+}
+
+void solve(){
+	int i,step,head,tail;
+	long long ans, *x, *y, *t;
+	for (i=1; i<=n; ++i) f1[i]=w[1]*h[i];
+	if (limit>n) limit=n;
+	if (limit==1){
+		cout<<f1[n]<<endl;
+		return;
 	}
-	void out(){
-		cout<<x<<' '<<y<<endl;
+	x=f1, y=f0, ans=f1[n];
+	for (step=2; step<=limit; ++step){
+		t=x; x=y; y=t;
+		head=1; tail=1; q[1]=step-1;
+		for (i=step; i<=n; ++i){
+			while (head<tail && (-h[i])*(w[q[head+1]+1]-w[q[head]+1])>(y[q[head+1]]-y[q[head]])) ++head;
+			x[i]=y[q[head]]+w[q[head]+1]*h[i];
+			while (head<tail && (y[q[tail]]-y[q[tail-1]])*(w[i+1]-w[q[tail]+1])<(y[i]-y[q[tail]])*(w[q[tail]+1]-w[q[tail-1]+1])) --tail;
+			q[++tail]=i;
+		}
+		if (x[n]<ans) ans=x[n];
 	}
-};
-
-int i,j,k,m,n,l;
-P a[N+10], b[N+10];
-int idx;
-ll f[N+10][M+10];
-deque<int> que[M+10];
-
-void gao(int i, int j, deque<int> & q){
-	if (sz(q)==0) return;
-	while (sz(q)>1){
-		int x=*q.begin(), y=*(q.begin()+1);
-		if (b[i].y*(b[x+1].x-b[y+1].x)>=f[y][j-1]-f[x][j-1]) q.pop_front();
-		else break;
-	}
-	int k=q.front();
-	f[i][j]=f[k][j-1]+b[i].y*b[k+1].x;
-
-
-	if (sz(que[j])<=1) que[j].pb(i);
-	else{
-		int x=*(q.end()-1), y=*(q.end()-2);
-		if ((f[i][j]-f[x][j])*(b[y+1].x-b[x+1].x)>=(f[x][j]-f[y][j])*(b[x+1].x-b[i+1].x)) que[j].pb(i);
-	}
+	cout<<ans<<endl;
 }
 
 int main(){
-	while (~scanf("%d%d", &n, &m)){
-		rep(i, n) a[i].in();
-		sort(a, a+n);
-		idx=1;
-		b[idx]=a[n-1];
-		repd(i, n-2, 0)
-			if (a[i].y>b[idx].y) b[++idx]=a[i];
-
-
-		repf(j, 0, m) que[j].clear();
-		que[0].pb(0);
-
-		clr(f, -1);
-		f[0][0]=0;
-
-		repf(i, 1, idx) repf(j, 1, min(i, m)){
-			gao(i, j, que[j-1]);			
-		}
-
-
-		ll ans=-1;
-		repf(j, 1, m) if (f[idx][j]!=-1)  
-			_checkmin(ans, f[idx][j]);
-		cout<<ans<<endl;
+	while (scanf("%d%d", &tn, &limit)==2){
+		init();
+		solve();
 	}
 	return 0;
 }
-
